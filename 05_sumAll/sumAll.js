6bc6b17604142610686d5f1aa73a041aa2ec3204
ben <ben.clark11@protonmail.com>
2023-01-11 14:56:48 +0000

    minor corrections

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -4,7 +4,7 @@ const sumAll = function(startNum, endNum) {
     let arrayOfString = [];
     let result = 0;
   // arrayOfNum elements to be converted from strings to int.
-  // the loop populates both strings and convers in one step.
+  // the loop populates both strings and converts in one step.
     for(let i = startNum; i <= endNum; i++){
       arrayOfString += arrayOfNum.push(parseInt(i));
     }
@@ -18,10 +18,3 @@ const sumAll = function(startNum, endNum) {
 
 // Do not edit below this line
 module.exports = sumAll;
-/*
-1 2 3 4
-
-1 + 2 = 3
-3 + 3 = 6
-6 + 4 = 10
-*/
