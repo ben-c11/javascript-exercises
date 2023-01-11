691ddab8f9fc31e721c2ea830e297699c3c40411
ben <ben.clark11@protonmail.com>
2023-01-11 14:51:37 +0000

    First step completed of sumAll function. The function returns the value of the sum of all numbers between and including the function arguments and so it has passed the first test. However the code can't account for iterating downwards so further testing is needed.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...num ) {
 
-};
+    let removedItems = [];
+    for (let k = 0; k < array.length * num.length; k++) {
+      for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < num.length; j++){
+          if (num[j] === array[i]){
+            removedItems = array.splice(i,1);
+          }
+        }
+      }
+    } 
+  return array;
+  
+  };
 
+// expect(removeFromArray([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
 // Do not edit below this line
 module.exports = removeFromArray;
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
 
+    arrayOfNum = [];
+    for(let i = startNum; i < endNum; i++){
+        arrayOfNum += i; 
+    }
+    console.log(arrayOfNum)
+    /*
+cant just do for(i = startNum; i < endNum; i++) as one test goes backwards.
+    
+    create array [start-end] 
+    then loop through the array
+
+  */  
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+/*
+1 2 3 4
+
+1 + 2 = 3
+3 + 3 = 6
+6 + 4 = 10
+*/
