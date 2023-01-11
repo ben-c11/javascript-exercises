52fe4ceb7dfd145e9f3aa9879eb60dfad404cb9f
ben <ben.clark11@protonmail.com>
2023-01-11 21:57:04 +0000

    05-sumAll complete Completed all 6 tests. The code could definitely use some improvements such as find ways to restructure the if else blocks and potentially group the error statements in a more organised fashion.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,20 +1,50 @@
 const sumAll = function(startNum, endNum) {
 
+    console.log('startNum: ' + startNum + ' Typeof: ' + typeof(startNum))
+    console.log('endNum: ' + endNum + ' Typeof: ' + typeof(endNum))
+    
     let arrayOfNum = [];
     let arrayOfString = [];
     let result = 0;
-  // arrayOfNum elements to be converted from strings to int.
-  // the loop populates both strings and converts in one step.
-    for(let i = startNum; i <= endNum; i++){
-      arrayOfString += arrayOfNum.push(parseInt(i));
-    }
+    
+    
+    if (typeof(startNum) === 'number'  && typeof(endNum) === 'number'){ 
+      if(startNum > 0 && endNum > 0){
+        if(endNum > startNum){
+          // arrayOfNum elements to be converted from strings to int.
+          // the loop populates both strings and convers in one step.
+          for(let i = startNum; i <= endNum; i++){
+            arrayOfString += arrayOfNum.push(parseInt(i));
+          }
   
-    for (let i = 0; i < arrayOfNum.length; i++){
-      result += arrayOfNum[i];
+          for (let i = 0; i < arrayOfNum.length; i++){
+            result += arrayOfNum[i];
+          }
+          
+          return result;
+  
+        } else if(endNum < startNum){
+          for(let i = startNum; i >= endNum; i--){
+            arrayOfString += arrayOfNum.push(parseInt(i));
+          }
+  
+          for (let i = 0; i < arrayOfNum.length; i++){
+            result += arrayOfNum[i];
+          }
+  
+          return result;
+        } 
+      }
+      else if(startNum < 0 || endNum < 0){
+        return 'ERROR';
+      } else {
+        
+        return 'ERROR';
+      } 
+    } else{
+      return 'ERROR';
     }
     
-    return result;
-};
-
+  };
 // Do not edit below this line
 module.exports = sumAll;
