bffc21a8e157ee263a1bf273908c467e94749821
ben <ben.clark11@protonmail.com>
2023-01-12 19:24:19 +0000

    07-tempConversion complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,28 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  temp = (temp - 32) * (5/9);
+
+  if (temp === 0){
+    return temp;
+  } else{
+    let roundedTemp = +temp.toFixed(1);
+
+    return roundedTemp;
+  }
 };
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+
+  temp = temp * (9/5) + 32;
+
+  if (Number.isInteger(temp)) {
+    return temp;
+  } else {
+    let roundedTemp = +temp.toFixed(1);
+
+    return roundedTemp;
+  }
+
+}
 
 // Do not edit below this line
 module.exports = {
