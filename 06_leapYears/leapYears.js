dc05367b772cc41faa43fb823cb722128ab4af32
ben <ben.clark11@protonmail.com>
2023-01-12 13:45:33 +0000

    06-leapYears Completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
+const leapYears = function(leapYear) {
+    /* divisible by 4. If divisible by 100 != leap
+    unless divisible by 400....
+    1900 - divisible by 4 and 100 not 400 != leap.*/
 
-};
+   if (leapYear % 4 === 0){
+      label: if (leapYear % 100 === 0 && leapYear % 400 === 0){
+        
+        break label;
+      } else if(leapYear % 100 === 0){
+        
+        return false;
+      }
+      
+      return true;
+    } else {
+      return false;
+    }
+
+  };
 
 // Do not edit below this line
 module.exports = leapYears;
