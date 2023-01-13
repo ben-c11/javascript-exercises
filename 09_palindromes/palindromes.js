1c48a7464eccd3bde9e0266232c3eee7e2a584a8
ben <ben.clark11@protonmail.com>
2023-01-13 13:12:37 +0000

    09-palindromes completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,44 @@
-const palindromes = function () {
+const palindromes = function (string) {
+      
+    string = string.toLowerCase();
+    string = removePunct(string);
+   
+    let palindrome = string;
+    
+    palindrome = palindrome.toLowerCase();
+  
+    palindrome = removePunct(palindrome);
 
-};
+    palindrome = reverseString(palindrome);
 
+    if (palindrome === string){
+      return true;
+    } else {
+      return false;
+    }
+    
+  }; 
+
+
+  const reverseString = function(string) {
+  
+    newString = '';
+    for (let i = string.length -1 ; i >= 0; i--){
+        newString = newString + string[i];
+      }
+      return newString;
+  };
+
+  function removePunct(string){
+    
+    let newString = '';
+
+    for(let i = 0; i < string.length; i++){
+      if (string.charCodeAt(i) > 47){
+        newString += string[i];
+      } 
+    }
+    return newString;
+  }
 // Do not edit below this line
 module.exports = palindromes;
