d96811d25de39a46e42371e9a30903c3f71e047d
ben <ben.clark11@protonmail.com>
2023-01-15 15:08:37 +0000

    11-getTheTitles.js completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,26 @@
-const getTheTitles = function() {
+const getTheTitles = function(bookArray) {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
+    
+    let bookTitleDescriptor;
+    let bookTitles = [];
 
-};
+    for(let i = 0; i < bookArray.length; i++){
+      
+      let bookTitleDescriptor = Object.getOwnPropertyDescriptor(books[i], 'title');
+      
+      bookTitles.push(bookTitleDescriptor.value);
+    }
+    return bookTitles;
+  };
 
 // Do not edit below this line
 module.exports = getTheTitles;
