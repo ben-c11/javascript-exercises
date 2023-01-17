1acc44002138056b1b4c0c57f92840bd070c2840
ben <ben.clark11@protonmail.com>
2023-01-17 14:03:38 +0000

    Progress on 12-findTheOldest Making progress figuring out how objects (and arrays of objects) can be worked with. This is a version one of the program where the return is the object value string name as opposed to the whole object. This means that the Jest tests will not successfully pass since the check uses 'findTheOldest(people).name' instead of 'findTheOldest(people)'.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
+ /*
+    Go into 'people', and iterate across whole array
+    Look into object and age = deathYear - birthYear
+        if no deathYear then deathYear == <today's date>
+    Compare age to previous object age, if greater, Oldest_ == current age
+    Continue for all objects in array
+    Print the object name.
+    */
 
-};
+    const findTheOldest = function(people) {
+        let oldestAge = 0;
+        let oldestPerson = people[0];
+  
+        for(let i = 0; i < people.length; i++){
+          let birthYear =  Object.getOwnPropertyDescriptor(people[i], 'yearOfBirth').value; 
+          let deathYear = Object.getOwnPropertyDescriptor(     people [i], 'yearOfDeath').value;
+          let personAge = deathYear - birthYear;
+  
+  
+          if(personAge > oldestAge){
+            oldestAge = personAge;
+            oldestPerson = Object.getOwnPropertyDescriptor(people[i], 'name').value;
+          }
+        }
+        return oldestPerson; 
+      }
+      console.log(findTheOldest(people))
 
 // Do not edit below this line
 module.exports = findTheOldest;
