7778ef7ba506b786cc5eb2766b30d260a2ae95cf
ben <ben.clark11@protonmail.com>
2023-01-28 12:37:23 +0000

    13-caesar completed Ensured that upper and lower parameters looped. Previously the codeChar iterations were being caught at the upper/lower bound by a misplaced >= or <=, changing these to > and <, respectively has fixed the issue. The code was also fully completed and fleshed out; it was important to me to ensure that the 'if' conditionals had multiple logical operators to prevent repeating code for both upper and lowercase letters. Doing so has allowed me to keep my code more concise as well as being easier to read.

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,48 @@
-const caesar = function() {
+const caesar = function(string, cipher) {
+    // charCodeAt() - gives the value of the letter.
+    let cipherText = '';
+    
 
-};
+    for (let i = 0; i < string.length; i++){
+      let charCode = string.charCodeAt(i); 
+    
+      // Check if character is a letter
+      if ((charCode >= 65 && charCode <= 90) || 
+          (charCode >= 97 && charCode <= 122)){
+      
+        if (cipher < 0){
+          // lower bound
+            let j = 0;
+
+            while(j > cipher){
+              if (charCode < 65 ||
+                 (charCode <= 97 && charCode >= 90)){
+                    
+                    charCode += 26;
+              }
+
+              charCode--;
+              j--;
+            }
+          } else{
+            // upper bound
+            let k = 0;
+
+            while (k < cipher){
+              if ((charCode >= 90 && charCode < 97) || charCode >= 122){
+                charCode -= 26;   
+              }
+
+              charCode++;
+              k++;
+            }
+          }
+        }
+      
+        cipherText += String.fromCharCode(charCode);
+      }
+      return cipherText;
+    }
 
 // Do not edit below this line
 module.exports = caesar;
